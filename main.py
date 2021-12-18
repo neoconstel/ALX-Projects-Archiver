4d0c432ceb477c6017510f38634af79428297440
neoconstel <neoconstel@gmail.com>
2021-12-18 21:20:19 +0100

    replace previous zip after scraping

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import zipper
 from alx_syllabus_scraper import *
 
 if __name__ == '__main__':
-    scape_output_directory = "alx_syllabus"
-    zip_output_file = f"{scape_output_directory}.zip"
+    scrape_output_directory = "alx_syllabus"
+    zip_output_file = f"{os.path.abspath(scrape_output_directory)}.zip"
 
-    scrape_alx_syllabus(scrape_output_directory=scape_output_directory)
-    zipper.zip_contents(scape_output_directory, zip_output_file)
+    scrape_alx_syllabus(scrape_output_directory=scrape_output_directory)
+
+    # delete any previous zipped output file TODO: only if SHA256 sum differs
+    if os.path.exists(zip_output_file):
+        os.remove(zip_output_file)
+
+    zipper.zip_contents(scrape_output_directory, zip_output_file)
