0cf851e90781ccba1f846500ae32abd788a81db4
neoconstel <neoconstel@gmail.com>
2022-02-06 13:19:15 +0100

    set to  deployment

--- alx_scrape_app/alx_scrape_view.py
+++ alx_scrape_app/alx_scrape_view.py
@@ -7,8 +7,8 @@ import redis
 from rq import Queue, Retry
 import time, datetime
 
-redis_cache = redis.Redis()  # for development while offline
-# redis_cache = redis.from_url(os.environ.get("REDIS_URL"))  # for deployment
+# redis_cache = redis.Redis()  # for development while offline
+redis_cache = redis.from_url(os.environ.get("REDIS_URL"))  # for deployment
 queue = Queue(connection=redis_cache, default_timeout=3600)
 
 alx_scrape_view = Blueprint("alx_scrape_view", __name__, template_folder="templates", static_folder="static")
