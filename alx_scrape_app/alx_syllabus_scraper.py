3283231d0431fa795e283c59bf41f9f63bf71883
neoconstel <neoconstel@gmail.com>
2022-03-11 13:09:14 +0100

    Boom! Added a contents html file that lists all the projects in chronological order from the earliest to latest project, and each listing appears as a clickable URL directing to the offline page of the project. Cool!

--- alx_scrape_app/alx_syllabus_scraper.py
+++ alx_scrape_app/alx_syllabus_scraper.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import os
 import json
 import time
+import re
+from datetime import datetime as dt
 
 
 def split_cookies(full_browser_cookie):
@@ -30,6 +32,7 @@ scrape_interval = 2  # Interval (in seconds) between requests sent.
 data_file = "scrape_data.dat"
 
 
+
 def scrape_alx_syllabus(scrape_output_directory="alx_syllabus", applied_cookies=browser_cookies, include_css=True, include_js=False):
 
     if not os.path.exists(data_file):
@@ -40,9 +43,21 @@ def scrape_alx_syllabus(scrape_output_directory="alx_syllabus", applied_cookies=
             scrape_data = json.load(save_file)
         except:
             scrape_data = {
-            "scraped_urls": []
+            "scraped_urls": [],
+            "contents": {
+
+
+                },
+            "cookies_hash": hash(applied_cookies)
             }
 
+    # if cookies are different from the previously used cookies, clear the
+    # contents data -- as they could have conflicting timestamps and end up
+    # sorting the appearance of the projects wrongly
+    if hash(applied_cookies) != scrape_data["cookies_hash"]:
+        scrape_data["contents"] = {}
+
+    
     for cookie_pair in split_cookies(applied_cookies):
         cookie_name = cookie_pair[0]
         cookie_value = cookie_pair[1]
@@ -86,8 +101,24 @@ def scrape_alx_syllabus(scrape_output_directory="alx_syllabus", applied_cookies=
 
             if not project_url in scrape_data["scraped_urls"]:
                 # proceed with scraping of project_url
-                with open(f"{section_dir}/{link_text}.html", 'w') as project_page:
+                project_page_path = f"{section_dir}/{link_text}.html"
+                with open(project_page_path, 'w') as project_page:
                     project_soup = BeautifulSoup(web_session.get(project_url).text, 'lxml')
+
+
+                    # Get project starting date and project title -- this would be used in sorting the order of the projects and creating a contents file
+                    date_regex = re.compile("\d\d-\d\d-\d\d\d\d")
+                    starting_date_match = re.search(date_regex, project_soup.select(".list-group-item")[2].text)
+                    if starting_date_match:
+                        starting_date = starting_date_match.group()
+                        # convert to epoch
+                        starting_date = dt.strptime(starting_date, "%m-%d-%Y").timestamp()
+
+                        project_title = project_soup.select_one("h1").text
+
+                        offline_contents_url = f'<a href="{section_title}/{link_text}.html">{project_title}</a>'
+                        scrape_data["contents"][starting_date] = offline_contents_url
+
 
                     # replace css online links with offline css files
 
@@ -184,6 +215,19 @@ def scrape_alx_syllabus(scrape_output_directory="alx_syllabus", applied_cookies=
                     time.sleep(scrape_interval)
 
             print(f"\n\n{link}")
+
+    # CONTENTS file creation (for chronological order of projects and offline location)
+    contents_path = f"{scrape_output_directory}/contents.html"
+    with open(contents_path, "w") as contents_file:
+        chronological_epoch = sorted(scrape_data["contents"].keys())
+
+        contents_file.write(f"<br>\n")
+        contents_file.write("<h1>Chronological order of Projects</h1>")
+        contents_file.write(f"<br>\n")
+        contents_file.write("<h3>(Projects in the same day are not ordered)</h3>")
+        for epoch in chronological_epoch:
+            contents_file.write(f"<br>\n")
+            contents_file.write(scrape_data["contents"][epoch])       
 
     print("\n\nScraping Completed")
     
